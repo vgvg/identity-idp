7e7ed040cfa6f318b22e570b987ca095a70006e0
Zach Margolis <zachary.margolis@gsa.gov>
2017-09-25 10:39:12 -0400

    Move downloading into scripts/activate

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,3 @@
-require 'login_gov/hostdata'
-
-root = File.expand_path('../../', __FILE__)
-
-LoginGov::Hostdata.in_datacenter do |hostdata|
-  hostdata.s3.download_configs(
-    '/%<env>s/idp/v1/application.yml' => File.join(root, 'config/application_s3.yml'),
-    '/%<env>s/idp/v1/database.yml'    => File.join(root, 'config/database_s3.yml')
-  )
-end
-
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
